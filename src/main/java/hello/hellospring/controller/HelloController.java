6073259da54db1e152448d992975d969ffa12c48
Mun-Seok <anstjr97@gmail.com>
2023-10-21 14:33:45 +0900

    SpringStudy(day2-API)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,33 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody                   // body에 데이터를 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // 객체를 전달 → HttpMessageConverter가 동작 (객체면 JsonConverter / 문자면 StringConverter가 동작하며 변환하여 서버에 보내줌)
+    }
+
+    static class Hello{
+        private String name;
+
+        // method를 통해 접근 - JavaBean 표준 방식
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
