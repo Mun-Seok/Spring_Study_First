789f4b57b05f90ee04489d5005851ec5be2cdae4
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-21 22:04:09 +0900

    MVC home page

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    // index.html 있지만 Controller가 우선순위이기 때문에 home이 호출됨
     @GetMapping("/")
     public String home(){
         return "home";
