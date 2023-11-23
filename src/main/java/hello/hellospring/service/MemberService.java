b6d4ac17d5817dbb73973e2663ed875b73008a4b
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-23 12:08:38 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -28,8 +28,21 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); // 중복 회원 검증
 
+//        long start = System.currentTimeMillis();
+//
+//        try{
+//            validateDuplicateMember(member); // 중복 회원 검증
+//
+//            memberRepository.save(member);
+//            return member.getId();
+//        } finally{
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("join = " + timeMs + "ms");
+//        }
+
+        validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
@@ -46,6 +59,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
+//        long start = System.currentTimeMillis();
+//        try{
+//            return memberRepository.findAll();
+//
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("findMembers = " + timeMs + "ms");
+//        }
         return memberRepository.findAll();
     }
 
